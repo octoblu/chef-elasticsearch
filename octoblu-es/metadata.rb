bf709a44e2c30321a826cebba0a6c796fe9dfc30
Jade Meskill and Roy van de Water <sqrt+jade+roy@octoblu.com>
2015-02-06 15:40:32 -0700

    add our own recipe

---
+++ octoblu-es/metadata.rb
@@ -0,0 +1,8 @@
+name        "octoblu elasticsearch"
+description 'elastic search stuff for octoblu'
+maintainer  "Jade Meskill"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends 'cron'
+depends 'logrotate'
